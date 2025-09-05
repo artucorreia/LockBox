24e8581be3c542ac4622667452c40f690aaca429
Arthur Correia <arthurcorria0109@gmail.com>
2025-09-05 19:14:50 -0300

    feat: generate project

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function Index() {
+  return (
+    <View style={{ backgroundColor: '#000', height: '100%', justifyContent: 'center', alignItems: 'center' }}>
+      <Text style={{ color: '#fff' }}>Hello, World!</Text>
+    </View>
+  );
+}
