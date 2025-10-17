946f4f6934cd67aa25a53727e52b9010937dc832
Arthur Correia <arthurcorria0109@gmail.com>
2025-10-17 19:20:51 -0300

    cleanup: remove unnecessary files

--- src/app/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from 'react-native';
-
-export default function Index() {
-  return (
-    <View style={{ backgroundColor: '#000', height: '100%', justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ color: '#fff' }}>Hello, World!</Text>
-    </View>
-  );
-}
